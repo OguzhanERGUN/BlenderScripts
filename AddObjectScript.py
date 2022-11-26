0d1b1e75076e66309dbebfdbcaee07035150a8c3
Oğuzhan Ergün <Oguzh4n15@hotmail.com>
2022-11-26 17:34:30 +0300

    BlenderFirstScript
    BlenderFirstScript

--- AddObjectScript.py
+++ AddObjectScript.py
@@ -11,10 +11,23 @@ class TestPanel(bpy.types.Panel):
         layout = self.layout
         
         row = layout.row()
-        row.label(text= "A little ghost", icon="GHOST_ENABLED")
+        row.label(text= "Add Some Object", icon="MATCUBE")
         row = layout.row()
-        row.operator("mesh.primitive_cube_add")
-        
+        row.operator("mesh.primitive_cube_add", icon='CUBE')
+        row = layout.row()
+        row.operator("mesh.primitive_cylinder_add", icon='MESH_CYLINDER')
+        row = layout.row()
+        row.operator("mesh.primitive_monkey_add", icon='MONKEY')
+        row = layout.row()
+        row.operator("object.text_add", icon='FILE_FONT')
+        row = layout.row()
+        row.operator("object.delete", icon='X',)
+
+
+
+
+
+
         
 def register():
     bpy.utils.register_class(TestPanel)
--- BlenderScriptDeneme.py
+++ BlenderScriptDeneme.py
@@ -1,3 +1,13 @@
 import bpy
 
-class ToolBar()
+class PanelClassName(bpy.types.Panel):
+    bl_idname = "panelname"
+    bl_label = "Panelname"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "category"
+
+    def draw(self, context):
+        layout = self.layout
+        
+    
